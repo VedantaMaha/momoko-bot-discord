0d479845f8433c98d661c9191ed0b8d319d90a7d
VedantaMaha <vedanta.maha@zettabyte.sg>
2020-07-31 15:31:47 +0800

    update comments

--- app.js
+++ app.js
@@ -1,26 +1,20 @@
-/* eslint-disable global-require */
-// require the discord.js module
 const Discord = require('discord.js');
 
-// add Node's native file system module
 const fs = require('fs');
-// add config data
 const { prefix, token } = require('./config.json');
-// create a new Discord client
+
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-// fs.readdirSync() method will return an array of all the file names in that directory
+// fs.readdirSync() method will return an array of all the file names in commands directory
 const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
 
 commandFiles.forEach((file) => {
   const command = require(`./commands/${file}`);
-  // set a new item in the Collection
+  // set a new item in the command Collection
   client.commands.set(command.name, command);
 });
 
-// when the client is ready, run this code
-// this event will only trigger one time after logging in
 client.once('ready', () => {
   console.log('Ready!');
 });
@@ -28,10 +22,18 @@ client.once('ready', () => {
 client.on('message', (message) => {
   // If the message either doesn't start with the prefix or was sent by a bot, exit early.
   if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  // example message: "momoko avatar @MomoBot @Mugi"
+  // prefix   = "momoko"
+  // command  = "avatar"
+  // args     = "@MomoBot @Mugi"
+
   // remove prefix and convert the rest to be array
   const args = message.content.slice(prefix.length).trim().split(/ +/);
-  // remove command word from args by using .shift()
+
+  // remove command from args by using .shift()
   const command = args.shift().toLowerCase();
+
   try {
     client.commands.get(command).execute(message, args);
   } catch (error) {
@@ -40,5 +42,5 @@ client.on('message', (message) => {
   }
 });
 
-// login to Discord with your app's token
+// login to Discord with app's token
 client.login(token);
